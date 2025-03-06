63a5d87128834035b82ddb399a8260b185e62220
Ro977 <itoomaronny@gmail.com>
2025-03-06 13:02:47 +0100

    Changement de chemin template

--- Take_risk/settings.py
+++ Take_risk/settings.py
@@ -78,7 +78,8 @@ ROOT_URLCONF = 'Take_risk.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # ✅ Chemin des templates,
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),  # Si un dossier templates global existe
+            os.path.join(BASE_DIR, 'shop', 'templates')],  # ✅ On force Django à voir shop/templates
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
